7b5430191acd0d989ba3ccac530e5cc4e9a3f70d
matthewb <matthewb@bellew.net>
2021-12-24 15:44:37 -0700

    mainstage

---
+++ src/python/MainStage.py
@@ -0,0 +1,46 @@
+from app.StrumPattern import StrumPattern
+from app.Application import *
+from app.Rhythms import RhythmModule, MpcDrumKit, MpcDrumTracks, Spark, VolcaBeats, MOTORIK3
+from app.Carpeggio import Carpeggio, CarpeggioRand, CarpeggioGenerative
+
+app = Application()
+q = app.patchQueue
+
+#
+# MODULES
+#
+
+StrumPattern(q, 'strumpattern_in', 'strumpattern_out')
+DebugModule(q, 'debug')
+RhythmModule(q, "rhythm_clock_in", "rhythm_cc_in", "rhythm_out", drumkit=Spark(60), channel=CH10, ppq=PPQ)
+CarpeggioGenerative(q, "carpeggio_clock_in", "carpeggio_cc_in", "carpeggio_out", channel=CH9, drone=CH8, ppq=PPQ, minor=False)
+
+# 
+# PATCH!
+#
+
+# USE THIS IF THERE IS NO EXTERNAL MIDI CLOCK
+app.patch('internal_clock','timekeeper_in')
+app.patch('knobs','internal_clock_cc')
+
+# get clock from MPC over IAC Bus
+#app.patch('IAC Driver Bus 1_clock','timekeeper_in')
+
+# sequencer needs clock, and supports control_change messages
+app.patch('clock','rhythm_clock_in')
+app.patch('knobs','rhythm_cc_in')
+# send to MPC
+app.patch('rhythm_out','instrument')
+#app.patch('rhythm_out','debug')
+
+# sequencer needs clock, and supports control_change messages
+app.patch('clock','carpeggio_clock_in')
+#app.patch('knobs','carpeggio_cc_in')
+# send to MPC
+app.patch('carpeggio_out','instrument')
+#app.patch('carpeggio_out','debug')
+
+# pass through keyboard to instrument for testing
+#self.patch('keyboard','instrument')
+
+app.main()
